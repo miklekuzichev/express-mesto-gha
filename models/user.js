8db8b1c4582e8cbbe65850e18e270e610b545416
miklekuzichev <dev310105@gmail.com>
2023-10-07 17:23:45 +0300

    Fix: Edit user.js after autotest

--- models/user.js
+++ models/user.js
@@ -6,19 +6,16 @@ const userSchema = new mongoose.Schema({
     required: true, // имя — обязательное поле
     minlength: 2, // минимальная длина имени — 2 символа
     maxlength: 30, // а максимальная — 30 символов
-    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
     required: true,
     minlength: 2,
     maxlength: 30,
-    default: 'Исследователь',
   },
   avatar: {
     type: String,
     required: true,
-    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
 });
 
